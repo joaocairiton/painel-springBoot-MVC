afa4a2ae09669fc346dfab79d667b0ded4689326
joaocairiton <joaocairiton@gmail.com>
2022-12-03 00:43:48 -0300

    foi adicionado o metodo remover usuario

--- src/main/java/com/cairiton/painel/api/controller/UsuarioController.java
+++ src/main/java/com/cairiton/painel/api/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -66,5 +67,21 @@ public class UsuarioController {
 		return ResponseEntity.ok(usuario);
 		
 	}
+	
+	
+	@DeleteMapping("/{usuarioId}")
+	public ResponseEntity<Void> remover(@PathVariable Long usuarioId){
+		
+		if (!usuarioRepository.existsById(usuarioId)) {
+			return ResponseEntity.notFound().build();
+			
+		}
+		
+		usuarioRepository.deleteById(usuarioId);
+		return ResponseEntity.noContent().build();
+		
+	}
+	
+	
 
 }
