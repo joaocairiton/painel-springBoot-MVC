52d0a5168e0307b473316f54d855e324b732290d
joaocairiton <joaocairiton@gmail.com>
2022-12-02 23:53:51 -0300

    foi adicionado metodo adicionar no usuario

--- src/main/java/com/cairiton/painel/api/controller/UsuarioController.java
+++ src/main/java/com/cairiton/painel/api/controller/UsuarioController.java
@@ -1,13 +1,16 @@
 package com.cairiton.painel.api.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cairiton.painel.domain.repository.UsuarioRepository;
@@ -41,6 +44,13 @@ public class UsuarioController {
 		 * 
 		 * return ResponseEntity.notFound().build();
 		 */
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Usuario adicionar(@RequestBody Usuario usuario) {
+		return usuarioRepository.save(usuario);
+
 	}
 
 }
