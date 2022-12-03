41a2951265140ab2d6dcecf562cfd588793fd265
joaocairiton <joaocairiton@gmail.com>
2022-12-03 00:30:07 -0300

    foi adicionado metodo atualizar no usuario

--- src/main/java/com/cairiton/painel/api/controller/UsuarioController.java
+++ src/main/java/com/cairiton/painel/api/controller/UsuarioController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -51,6 +52,19 @@ public class UsuarioController {
 	public Usuario adicionar(@RequestBody Usuario usuario) {
 		return usuarioRepository.save(usuario);
 
+	}
+	
+	@PutMapping("/{usuarioId}")
+	public ResponseEntity<Usuario> atualizar (@PathVariable Long usuarioId,@RequestBody  Usuario usuario) {
+		
+		if (!usuarioRepository.existsById(usuarioId)) {
+			return ResponseEntity.notFound().build();
+			
+		}
+		usuario.setId(usuarioId);
+		usuario = usuarioRepository.save(usuario);
+		return ResponseEntity.ok(usuario);
+		
 	}
 
 }
