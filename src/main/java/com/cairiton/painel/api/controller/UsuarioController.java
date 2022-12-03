3f9efc6cb95bb68528a05f38f0faf51312c06fc0
joaocairiton <joaocairiton@gmail.com>
2022-12-02 23:01:58 -0300

    foi adicionado metodo buscar usuario por id

--- src/main/java/com/cairiton/painel/api/controller/UsuarioController.java
+++ src/main/java/com/cairiton/painel/api/controller/UsuarioController.java
@@ -1,36 +1,46 @@
 package com.cairiton.painel.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cairiton.painel.domain.repository.UsuarioRepository;
 import com.cairiton.painel.model.Usuario;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
-	
-	@GetMapping("/usuarios")
-	public List<Usuario> listar (){
-		
-		Usuario usuario1 = new Usuario();
-		usuario1.setId(1L);
-		usuario1.setNome("joao");
-		usuario1.setEmail("joao@gmail.com");
-		usuario1.setSenha("123456");
-		usuario1.setTelefone("62 91111-1111");
-		
-		Usuario usuario2 = new Usuario();
-		usuario2.setId(2L);
-		usuario2.setNome("bastiao");
-		usuario2.setEmail("bastiao@gmail.com");
-		usuario2.setSenha("123456");
-		usuario2.setTelefone("62 92222-2222");
-		
-		return Arrays.asList(usuario1, usuario2);
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	@GetMapping
+	public List<Usuario> listar() {
+		return usuarioRepository.findAll();
 	}
 
-	
+	@GetMapping("/{usuarioId}")
+	public ResponseEntity<Usuario> buscar(@PathVariable Long usuarioId) {
+		return usuarioRepository.findById(usuarioId)
+				// .map(usuario -> ResponseEntity.ok(usuario))
+				.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+
+		/*
+		 * Optional<Usuario> usuario = usuarioRepository.findById(usuarioId);
+		 * 
+		 * if (usuario.isPresent()) { return ResponseEntity.ok(usuario.get()); }
+		 * 
+		 * return ResponseEntity.notFound().build();
+		 */
+	}
+
 }
--- src/main/java/com/cairiton/painel/model/Usuario.java
+++ src/main/java/com/cairiton/painel/model/Usuario.java
@@ -1,16 +1,26 @@
 package com.cairiton.painel.model;
 
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
-
+@Entity
 public class Usuario {
 
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nome;
 	private String email;
 	private String senha;
